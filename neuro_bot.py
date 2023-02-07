2ea45f5b79dc46bec632d4bb2184c7d2281e17e0
Тимур Комолов <komolov.timurka@mail.ru>
2023-02-07 12:10:32 +0300

    base is ready

---
+++ neuro_bot.py
@@ -0,0 +1,90 @@
+import vk_api
+import os
+import random
+import json
+import pickle
+import urllib.request
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.neural_network import MLPClassifier
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
+from neuro_defs import *
+from threading import Thread
+
+
+
+if __name__ == "__main__":
+    if not os.path.isfile('model.pkl'):
+        with open('intents_dataset.json', 'r', encoding='UTF-8') as f:
+            data = json.load(f)
+        X = []
+        y = []
+
+        for name in data:
+            for question in data[name]['examples']:
+                X.append(question)
+                y.append(name)
+            for phrase in data[name]['responses']:
+                X.append(phrase)
+                y.append(name)
+
+        # векторизируем файлы и обучаем модель
+
+        vectorizer = CountVectorizer()
+        X_vec = vectorizer.fit_transform(X)
+        model_mlp = MLPClassifier()
+        model_mlp.fit(X_vec, y)
+        with open('model.pkl', 'wb') as f:
+            pickle.dump(model_mlp, f)
+        print("Обучено")
+    else:
+        with open('model.pkl', 'rb') as f:
+            model_mlp = pickle.load(f)
+        print("Обученная модель загружена")
+    Thread(target=add_answer, args=()).start()
+    if os.path.isfile('mirea_users.pickle'):
+        with open('mirea_users.pickle', 'rb') as f:
+            users = pickle.load(f)
+
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            id = event.user_id
+            if not (id in users):  # если нет в базе данных
+                users[id] = UserInfo()
+                with open('mirea_users.pickle', 'wb') as f:
+                    pickle.dump(users, f)
+            message = clean_up(event.text)  # очищенный текст
+            if users[id].state == "Пожелания":
+                if text_match(message, "отмена"):
+                    users[id].state = ""
+                    send_message(id, "Пожелание отменено")
+                else:
+                    users[id].state = ""
+                    for i in users.keys():
+                        if users[i].name == "ser13volk":
+                            user_get = vk.users.get(user_ids=id)
+                            first_name = user_get['first_name']
+                            last_name = user_get['last_name']
+                            send_message(i, "Пользователь " + first_name + " " + last_name + " хочет сказать: " + event.text)
+                    send_message(id, "Спасибо за ваше пожелание!")
+            if text_match(message,"расписание"):
+                send_message(id,"В данном боте тестируется только система ответов на вопросы, расписание в основной версии ВИКА")
+            elif text_match(message,"карта"):
+                send_message(id,
+                             "В данном боте тестируется только система ответов на вопросы, карта в основной версии ВИКА")
+            elif text_match(message,"рассылка"):
+                send_message(id,"В данном боте тестируется только система ответов на вопросы, рассылка в основной версии ВИКА")
+            elif text_match(message,"Расписание пересдач"):
+                send_message(id,"В данном боте тестируется только система ответов на вопросы, расписание пересдач в основной версии ВИКА")
+            elif text_match(message,"Что ты умеешь"):
+                send_message(id,"Напишите мне любой вопрос, связанный с нашим университетом, и я постараюсь найти ответ на него. Учтите, что я не живой "
+                                "человек и могу ошибаться, однако в этой версии база ответов значительно расширена, а система "
+                                "распознавания вопросов улучшена.")
+            elif text_match(message,"Пожелания по улучшению"):
+                send_message(id,"Введите в следующем сообщении свои пожелания по улучшению бота. Они будут переданы разработчику. Если хотите отменить отправку, напишите 'Отмена'")
+                users[id].state = "Пожелания"
+            else:
+
+                create_keyboard(id, answering(message, model_mlp, vectorizer, data))
---
+++ neuro_defs.py
@@ -0,0 +1,138 @@
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
+import vk_api
+from private_api import token_api  # токен который не должен быть у всех,поэтому вынес в отдельную функцию
+import nltk
+import re
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.neural_network import MLPClassifier
+import random
+import json
+
+
+vk_session = vk_api.VkApi(token=token_api)
+vk = vk_session.get_api()
+
+
+# класс с информацией о пользователе, чтобы можно было запоминать информацию о пользователе, в том числе о рассылках.
+# не знаю, как это реализовано в основном боте, поэтому пусть будет так
+class UserInfo:
+    def __init__(self):
+        # предложение запоминать группу пользователя, чтобы не вводить её каждый раз
+        self.name = ""  # заглушка чтобы отправлять сообщения создателю
+        self.group = ""
+        self.state = ""  # состояние пользователя, чтобы понимать, что ему нужно сделать
+        self.sending = []
+        # условно изначальные рассылки присвоить False, чтобы не рассылал, сделано скорее как заглушка
+        for i in range(11):
+            self.sending.append(False)
+
+
+def send_message(id, msg, stiker=None, attach=None):
+        try:
+            vk.messages.send(
+                user_id=id,
+                random_id=get_random_id(),
+                message=msg,
+                sticker_id=stiker,
+                attachment=attach
+            )
+        except BaseException:
+            print("ошибка, возможно человек добавил в чс")
+            return
+
+
+# на данный момент только одна клавиатура, но есть возможность создавать другие
+def create_keyboard(id, text, response="start"):
+    try:
+        print(2)
+        if response == "start":
+            keyboard = VkKeyboard(one_time=False)
+            keyboard.add_button('Расписание', color=VkKeyboardColor.PRIMARY)
+            keyboard.add_line()
+            keyboard.add_button('Карта университета', color=VkKeyboardColor.PRIMARY)
+            keyboard.add_line()
+            keyboard.add_button('Рассылка', color=VkKeyboardColor.PRIMARY)
+            keyboard.add_line()
+            keyboard.add_button('Расписание пересдач', color=VkKeyboardColor.PRIMARY)
+            keyboard.add_line()
+            keyboard.add_button('Что ты умеешь?', color=VkKeyboardColor.PRIMARY)
+            keyboard.add_line()
+            keyboard.add_button('Пожелания по улучшению', color=VkKeyboardColor.PRIMARY)
+            keyboard = keyboard.get_keyboard()
+            vk.messages.send(
+                user_id=id,
+                random_id=get_random_id(),
+                message=text, keyboard=keyboard)
+    except BaseException:
+        print("ошибка, возможно человек добавил в чс")
+        return
+
+
+def clean_up(text):
+    text = text.lower()
+    # описываем текстовый шаблон для удаления: "все, что НЕ является буквой \w или пробелом \s"
+    re_not_word = r'[^\w\s]'
+    # заменяем в исходном тексте то, что соответствует шаблону, на пустой текст (удаляем)
+    text = re.sub(re_not_word, '', text)
+    return text
+
+
+def text_match(user_text, example):
+    user_text = clean_up(user_text)
+    example = clean_up(example)
+    # A.find(B) - возвращает номер буквы, с которой идет текст B внутри текста А
+    # find возвращает -1 в случае, если ничего не нашла
+    if user_text.find(example) != -1:
+        return True
+    if example.find(user_text) != -1:
+        return True
+    example_len = len(example)
+    difference = nltk.edit_distance(user_text, example)
+    return (difference / example_len) < 0.4
+
+
+users = {}
+
+
+def get_intent(text, model_mlp):
+    vectorizer = CountVectorizer()
+    text_vec = vectorizer.transform([text])
+    return model_mlp.predict(text_vec)[0]
+
+
+def get_response(intent, data):
+    return random.choice(data[intent]['responses'])
+
+
+def answering(text, model_mlp, vectorizer, data):
+    intent = get_intent(text, model_mlp, vectorizer)
+    answer = get_response(intent, data)
+    return answer
+
+
+def add_answer():
+    with open('intents_dataset.json', 'r', encoding='UTF-8') as f:
+        data = json.load(f)
+    while True:
+        print("Введите название интента")
+        intent = input()
+        print("Вводите вопросы, чтобы закончить, введите 0")
+        while True:
+            question = input()
+            if question == "0":
+                break
+            data[intent]['examples'].append(question)
+        print("Вводите ответы, чтобы закончить, введите 0")
+        while True:
+            answer = input()
+            if answer == "0":
+                break
+            data[intent]['responses'].append(answer)
+        with open('intents_dataset.json', 'w', encoding='UTF-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        print("Ответ был записан в файл. Продолжайте вводить")
+
+
+
+
